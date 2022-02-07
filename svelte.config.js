f958250f5fa7a839ba19f598411e33fd041ad11d
Matthew Wong <wongchiking@gmail.com>
2022-02-08 01:14:24 +0800

    add github static page configurations, add docs folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+// Consult pages below for github pages hosting static pages
+// https://github.com/sveltejs/kit/tree/master/packages/adapter-static 
+// https://sveltesaas.com/articles/sveltekit-github-pages-guide/
+
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
+
 import preprocess from 'svelte-preprocess';
 import path from 'path';
+
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +18,16 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		// configure for github pages, set to produce static files under "docs" folder 
+		// adapter: adapter(),
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : '/simple-math-worksheet-generator',
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
