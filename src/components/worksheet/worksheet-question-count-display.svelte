249f44ea08553e0b18077ccdb552c14ce36b35f1
Matthew Wong <wongchiking@gmail.com>
2022-02-16 14:55:47 +0800

    display total pages generated

--- src/components/worksheet/worksheet-question-count-display.svelte
+++ src/components/worksheet/worksheet-question-count-display.svelte
@@ -1,10 +1,16 @@
 <script>
     // import stores
     import { worksheetDataCountStore } from '$stores/worksheet-data-stores'; 
+    import { questionsPerPageStore } from '$stores/questions-per-page-stores';
+
+    let totalPages = 0;
+    $ : {
+        totalPages = Math.ceil($worksheetDataCountStore / $questionsPerPageStore);
+    }
 </script>
 
 {#if $worksheetDataCountStore > 0}
-    <h5>Total {$worksheetDataCountStore} questions are generated.</h5>
+    <h5>Total {$worksheetDataCountStore} questions ({totalPages} pages) are generated.</h5>
 {:else}
     <h5>No questions are generated.</h5>
 {/if}    
