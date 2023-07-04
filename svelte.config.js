da2c930299d1661d5acf15b6f58b0df9c3d56b62
Matthew Wong <wongchiking@gmail.com>
2023-07-05 01:04:33 +0800

    recreate project with sveltekit 1.20.4, migrate existing code

--- svelte.config.js
+++ svelte.config.js
@@ -1,47 +1,29 @@
-// Consult pages below for github pages hosting static pages
-// https://github.com/sveltejs/kit/tree/master/packages/adapter-static 
-// https://sveltesaas.com/articles/sveltekit-github-pages-guide/
+/* import adapter from '@sveltejs/adapter-auto'; */
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-static";
-import { optimizeImports, optimizeCss } from "carbon-preprocess-svelte";
-
-import preprocess from 'svelte-preprocess';
-import path from 'path';
-
-
-const isDevEnv = process.env.NODE_ENV === 'development';
 const isProdEnv = process.env.NODE_ENV === 'production';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [preprocess(), optimizeImports()],
+	preprocess: vitePreprocess(),
 
 	kit: {
-		// configure for github pages, set to produce static files under "docs" folder 
-		// adapter: adapter(),
-		adapter: adapter({
-			pages: "docs",
-			assets: "docs"
-		}),
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+
 		paths: {
-			// change below to your repo name
-			base: isDevEnv ? '' : '/simple-math-worksheet-generator',
+			base: process.env.NODE_DEV === 'production' ? '/simple-math-worksheet-generator' : ''
 		},
 
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					$components: path.resolve('./src/components'),
-					$stores: path.resolve('./src/stores'),
-					$utils: path.resolve('./src/utils')
-				}
-			},
-			plugins: [/*isProdEnv && optimizeCss()*/]
+		alias: {
+			$components: "src/lib/components",
+			$stores: "src/lib/stores",
+			$utils: "src/lib/utils"
 		}
 	}
 };
