7298ff026165ad97e4e5f5da6ca18b18a9369bc2
umarimran1234 <umartkd989@gmail.com>
2024-12-23 22:45:29 +0500

    update

---
+++ src/routes/whatisyourGrad/+page.svelte
@@ -0,0 +1,55 @@
+
+<section class="bg-gray-50 dark:bg-gray-900 h-screen flex items-center justify-center">
+    <div class="flex flex-col md:flex-row  w-full  items-center justify-center   ">
+    
+
+      <!-- Right Side Form Section -->
+      <div class="  w-full h-screen p-12  rounded-lg dark:border dark:bg-gray-800 flex flex-col justify-center  dark:border-gray-700  ">
+        
+        <div class=" text-center  ">
+
+            <h1 class="text-3xl font-bold text-gray-900 dark:text-white text-center mb-4">
+             Information
+            </h1>
+        </div>
+         <form >
+           
+          <div class="flex flex-col justify-between w-full  gap-2" >
+            <input placeholder="name" class="w-full flex items-center gap-5 text-black border-2 bg-transparent font-medium rounded-lg text-sm px-5 py-2.5"/>
+              
+              
+
+            <select placeholder="Grade" class="block appearance-none w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 pr-8 rounded-lg shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500">
+                <option value="" disabled selected>Select Grade</option>
+                <option value="1">Grade 1</option>
+                <option value="2">Grade 2</option>
+                <option value="3">Grade 3</option>
+                <option value="4">Grade 4</option>
+                <option value="5">Grade 5</option>
+                <option value="6">Grade 6</option>
+                <option value="7">Grade 7</option>
+                <option value="8">Grade 8</option>
+                <option value="9">Grade 9</option>
+                <option value="10">Grade 10</option>
+                <option value="11">Grade 11</option>
+                <option value="12">Grade 12</option>
+              </select>
+              
+      
+              
+            
+              
+            
+          </div>
+          <div class="flex items-center gap-4 justify-center">
+            <!-- First Div -->
+    
+          </div>
+          
+          
+         </form>
+     </div>
+    
+    </div>
+  </section>
+  
